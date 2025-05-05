a95f03d565317a0da37f767db6f7eb5d11885ed8
Volodymyr Palazanov <volodymyr.palazanov@itdesk.eu>
2025-05-05 18:37:51 +0200

    set status to 404-not found if there is no data for email

--- app/Http/Controllers/SellasistController.php
+++ app/Http/Controllers/SellasistController.php
@@ -28,7 +28,7 @@ class SellasistController extends Controller
         ]);
 
         if (!$response->ok()) {
-            return response()->json(['error' => 'Failed to fetch data for: ' . $email], 500);
+            return response()->json(['error' => 'Failed to fetch data for: ' . $email], 404);
         }
 
         $orders = collect($response->json())
